9f7cc2e799b4b9f34988a6cc7739483d03148db6
Angel Choquehuanca <angel.y.ch.p@gmail.com>
2018-05-11 08:32:14 -0500

    Corrigiendo 1

--- FactSunat/src/Frames/menu.java
+++ FactSunat/src/Frames/menu.java
@@ -265,7 +265,7 @@ public class menu extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Cant.", "Descripcion", "P.Unitario", "Importe"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
